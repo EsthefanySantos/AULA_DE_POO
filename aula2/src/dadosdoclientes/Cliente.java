6ac87c5fd27639d9d15417ca3b59596592670b4f
Esthefany <tetesantos116@gmail.com>
2026-03-06 13:04:53 -0300

    Primeira aula da facul de poo

---
+++ aula2/src/dadosdoclientes/Cliente.java
@@ -0,0 +1,55 @@
+package dadosdoclientes;
+
+public class Cliente {
+
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String email;
+
+    public Cliente(String nome, int idade, String cpf, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void Imprime() {
+        System.out.println("Nome do cliente: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Email: " + this.email);
+    }
+}
